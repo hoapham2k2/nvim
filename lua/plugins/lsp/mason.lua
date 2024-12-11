de8f009be1fa70017547071935f87d62fa5c1170
hoapham2k2 <20520995@gm.uit.edu.vn>
2024-12-11 16:54:05 +0700

    update

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,20 +1,22 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		require("mason").setup()
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        require("mason").setup()
 
-		local ensure_installed = {
-			-- Add your language servers and tools here
-			"lua_ls",
-			"stylua",
-		}
+        local ensure_installed = {
+            -- Add your language servers and tools here
+            "lua_ls",
+            "stylua",
+            "omnisharp",
+            "omnisharp-mono",
+        }
 
-		require("mason-tool-installer").setup({
-			ensure_installed = ensure_installed,
-		})
-	end,
+        require("mason-tool-installer").setup({
+            ensure_installed = ensure_installed,
+        })
+    end,
 }
